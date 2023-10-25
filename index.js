e26cdb69faae10c028c6fb583a90e3d11f157450
causadev <causadev@proton.me>
2023-10-25 02:30:10 -0500

    Bad code needs improvement💩💩💩 idk when

--- index.js
+++ index.js
@@ -1,107 +1,82 @@
-const turns = {
-  X: "×",
-  O: "o"
+const Player = (mark) => {
+  return {
+    mark
+  }
 }
-
-
-const user = (name) => {
-  const userName = "@" + name
-  return { name, userName }
-}
-
 
 const gameBoard = (() => {
-  let running = true;
-  let currentPlayer = turns.X
-  let board = ["", "", "", "", "", "", "", "", ""];
+  const board = Array(9).fill(null)
+  const cells = document.querySelectorAll(".cell")
+  const gameStatus = document.querySelector(".gameStatus")
+  const restartBtn = document.querySelector(".restart")
+  const playAgainBtn = document.querySelector(".play");
+  const squareX = document.querySelector(".squareX");
+  const squareO = document.querySelector(".squareO")
   const modal = document.querySelector(".modal");
   const overlay = document.querySelector("#overlay")
-  const form = document.querySelector("form")
-  const input = document.querySelector("input");
-  const name = document.querySelector("p")
+  let currentPlayer = Player("x")
+  let gameActive = true
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    name.textContent = `Name: ${input.value}`
-  })
+  const playAgain = () => {
+    restartGame()
+  }
+
+  const changePlayer = () => {
+    currentPlayer.mark = currentPlayer.mark === "x" ? "o" : "x"
+  }
+
+  const renderMark = (cell, index) => {
+    board[index] = currentPlayer.mark
+    cell.textContent = currentPlayer.mark
+  }
+
   const openModal = () => {
     modal.classList.add("active")
     overlay.classList.add("active")
   }
-
-
-
   const closeModal = () => {
     modal.classList.remove("active")
     overlay.classList.remove("active")
   }
 
-  const renderBoard = () => {
-    const cells = document.querySelectorAll(".cell");
-    cells.forEach((cell) => cell.addEventListener("click", handleCellClicked));
-    const restartBtn = document.querySelector(".restart");
-    restartBtn.addEventListener("click", restartGame)
-  };
-
-
-  function handleCellClicked() {
-    const cellClicked = this.getAttribute("data-cell");
-
-    if (board[cellClicked] !== "" || !running) {
-      return
+  cells.forEach((cell, index) => cell.addEventListener("click", () => {
+    if (board[index] !== null || !gameActive) return
+    renderMark(cell, index)
+    const winner = checkWinner(board);
+    if (winner) {
+      openModal()
+      gameStatus.textContent = `${winner} winner🎉`
+      gameActive = false
+      playAgainBtn.addEventListener("click", playAgain)
+    } else if (!board.includes(null)) {
+      openModal()
+      playAgainBtn.addEventListener("click", playAgain)
+      gameStatus.textContent = `draw 😴`
+      gameActive = false
+    } else {
+      changePlayer()
+      displayController.activePlayer()
     }
+  }))
 
-    renderMark(this, cellClicked)
-    chekWinner()
-  }
-
-
-  const renderMark = (cell, index) => {
-    board[index] = currentPlayer
-    cell.textContent = currentPlayer
-    activePlayer();
-  }
-
-
-  const changePlayer = () => {
-    currentPlayer = currentPlayer === turns.X ? turns.O : turns.X
-  }
 
   const activePlayer = () => {
-    const squareX = document.querySelector(".squareX");
-    const squareO = document.querySelector(".squareO");
-
-
-    if (currentPlayer === turns.X) {
+    if (squareX.textContent === currentPlayer.mark) {
       squareX.style.background = "#776e62"
       squareO.style.background = "#181a1b"
       squareX.style.transition = "all 0.2s ease-in-out";
-    } else if (currentPlayer === turns.O) {
+    } else if (squareO.textContent === currentPlayer.mark) {
       squareO.style.background = "#776e62"
       squareX.style.background = "#181a1b"
       squareO.style.transition = "all 0.2s ease-in-out";
     }
   }
 
+  activePlayer()
 
 
-
-  const restartGame = () => {
-    const cells = document.querySelectorAll(".cell");
-    cells.forEach((cell) => cell.textContent = "")
-    board = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = turns.X
-    closeModal()
-  }
-
-
-
-
-  const chekWinner = () => {
-    const gameStatus = document.querySelector(".gameStatus")
-    const playAgainBtn = document.querySelector(".play")
-    let roundWon = false
-    const winCombos = [
+  const checkWinner = (squares) => {
+    const lines = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -109,45 +84,31 @@ const gameBoard = (() => {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6],
+      [2, 4, 6]
     ]
-    for (let i = 0; i < winCombos.length; i++) {
-      const condition = winCombos[i];
-      const cellA = board[condition[0]];
-      const cellB = board[condition[1]];
-      const cellC = board[condition[2]];
 
-      if (cellA == "" || cellB == "" || cellC == "") {
-        continue;
-      }
-      if (cellA == cellB && cellB == cellC) {
-        roundWon = true;
-        break;
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
       }
     }
-
-    if (roundWon) {
-      gameStatus.textContent = `${input.value} winner: ${currentPlayer} 🏆`
-      openModal()
-      playAgainBtn.addEventListener("click", () => {
-        restartGame()
-        closeModal()
-      })
-    } else if (!board.includes("")) {
-      gameStatus.textContent = `draw`
-      openModal()
-      playAgainBtn.addEventListener("click", () => {
-        restartGame()
-        closeModal()
-      })
-    } else {
-      changePlayer()
-    }
+    return null
   }
 
 
-  renderBoard()
+
+
+  const restartGame = () => {
+    board.fill(null)
+    cells.forEach((cell) => cell.textContent = "")
+    gameStatus.textContent = ""
+    gameActive = true
+    currentPlayer.mark = "x"
+    closeModal()
+  }
+  restartBtn.addEventListener("click", restartGame)
+
+
 
 })();
-
-
